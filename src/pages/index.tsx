259d45b77e8df47f790d59ed6d6a0d4353fdfa54
zhangming <qianzm2490@vip.qq.com>
2020-03-31 20:08:41 +0800

    chore: upgrade to umi 3.x

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './index.less';
 
-export default function() {
+export default () => {
   return (
-    <div className={styles.container}>
-      index
+    <div>
+      <h1 className={styles.title}>Page index</h1>
     </div>
   );
-}
+};
--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,3 @@
 declare module '*.css';
 declare module '*.less';
 declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
